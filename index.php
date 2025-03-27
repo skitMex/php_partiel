8bae37c654e93b67e568ffce1c3576e66bfc9541
Martin Hurvois <martin@hurvois.fr>
2025-03-27 14:29:21 +0100

    commit bdd (changement

--- index.php
+++ index.php
@@ -27,22 +27,14 @@ $resPerso = $sqlPerso->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des mangas</title>
 </head>
-<h1>Top manga: </h1>
 <ul class="manga">
-    <li id="Jojo"Jojo's part 7>
-    <p> 2004</p>
-    </li>
-    <li id="Vagabon" vagabond>
-    <p> 1998</p>
-    </li>
-    <li id="one piece"one piece>
-    <p> 1997</p>
-    </li>
-    <li id="Monster"Monster>
-    <p> 1994</p>
-    </li>
-    <li id="Berserk"Berserk>
-    <p> 1989</p>
-    </ul>
+    <?php foreach ($mangas as $manga): ?>
+        <li>
+            <a href="index.php"><?= htmlspecialchars($manga['titre']) ?></a>
+            <p><?= htmlspecialchars($manga['annee de pub']) ?></p>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
 
  
